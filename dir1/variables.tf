ae290291ca59a87e0e037c664e9364ec9b0e0745
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-10-27 15:57:49 +0900

    Add dir1, dir2

---
+++ dir1/variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "username" {
+  description = "admin username"
+}
+
+variable "password" {
+  description = "admin password"
+}
+
+variable "instance_name" {
+  description = "vm instance name"
+}
+
+variable "additional_tags" {
+  default = {}
+  description = "Additional Tags"
+}
---
+++ dir2/terraform.tfvars
@@ -0,0 +1,10 @@
+prefix = "qa"
+location = "koreacentral"
+username = "provisioning_catalog_hyein"
+password = "dksThdud1!3@"
+instance_name = "provisioningcatalogazure"
+additional_tags = {
+  "ManagedBy" = "Dev OS"
+  "CreatedBy" = "hyein"
+  "Directory" = "dir2"
+}
